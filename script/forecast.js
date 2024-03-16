422cbce01d5bfadba2a10318b7a5854fceea8f94
Madiyar <madiyar.galymbek@gmail.com>
2024-03-16 20:48:08 +0500

    thurd commit

--- script/forecast.js
+++ script/forecast.js
@@ -2,7 +2,7 @@ const key = '8rkaw8Gm5KjmiB5gAzvN5zns5pIkzNZM';
   
   const getWeather = async(id) => {
 
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
     const query =  `${id}?apikey=${key}`;
 
     const response = await fetch(base + query);
@@ -13,7 +13,7 @@ const key = '8rkaw8Gm5KjmiB5gAzvN5zns5pIkzNZM';
 
   const getCity = async(city) => {
     
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
     const query =  `?apikey=${key}&q=${city}`;
 
     const response = await fetch(base + query);
